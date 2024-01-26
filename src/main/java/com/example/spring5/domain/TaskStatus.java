5b527671f93c012f9a7277101f1bad7808d9e7fc
Diman76 <Dmitiy_yar@mail.ru>
2024-01-26 12:27:12 +0300

    init

---
+++ src/main/java/com/example/spring5/domain/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.example.spring5.domain;
+
+public enum TaskStatus {
+    NOT_STARTED, IN_PROGRESS, COMPLETED;
+}
---
+++ src/main/java/com/example/spring5/repository/TasksRepository.java
@@ -0,0 +1,44 @@
+package com.example.spring5.repository;
+
+import com.example.spring5.domain.Task;
+import com.example.spring5.domain.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Интерфейс для работы с Базой данных.
+ */
+
+@Repository
+public interface TasksRepository extends JpaRepository<Task, Long> {
+
+    /**
+     * Получить список задач по статусу.
+     * @param taskStatus
+     * @return лист задач.
+     */
+
+    @Query("Select t from Task t where t.taskStatus = :taskStatus")
+    List<Task> findByStatus(TaskStatus taskStatus);
+
+    /**
+     * Обновить статус задачи.
+     * @param taskStatus
+     * @param id
+     */
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Task SET taskStatus = :taskStatus WHERE id = :id")
+    void changeTask(TaskStatus taskStatus, Long id);
+
+
+
+
+
+}
